6e285d3288e8951aa86466aab3fe42256cb61605
Thiciane <yakugort@gmail.com>
2026-06-09 15:30:59 -0300

    Atualizado lin76

--- api/db.js
+++ api/db.js
@@ -2,20 +2,21 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const BD = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'admin',
-    database: 'db_agro_finan', 
-    port: 5432
+  connectionString: "postgresql://postgres.zealpzdccmirmqkyfmsz:Thiciane12345@aws-1-us-east-1.pooler.supabase.com:6543/postgres?pgbouncer=true",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const testarConexao = async () => {
-    try {
-        const cliente = await BD.connect(); 
-        cliente.release(); 
-    } catch (error) {
-        throw new Error(error.message);
-    }
+  try {
+    const cliente = await BD.connect();
+    console.log("✅ Conectado ao banco com sucesso!");
+    cliente.release();
+  } catch (error) {
+    console.error("❌ Erro ao conectar no banco:", error.message);
+    throw new Error(error.message);
+  }
 };
 
 export { BD, testarConexao };
